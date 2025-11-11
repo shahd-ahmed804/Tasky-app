f1a451c5b9e9fe63260e7f5749b3b6ef749dbc4d
shahd_ahmed <shahdahmed8883@gmail.com>
2025-11-11 15:37:29 +0200

    add splash_screen.dart

---
+++ lib/core/constant/assets_constant.dart
@@ -0,0 +1,4 @@
+abstract class AssetsConstant{
+  static String TaskIcon = "assets/icons/Task_icon.png";
+  static String YIcon = "assets/icons/y_icon.png";
+}
--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,4 +1,7 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
+import 'package:tasky_app/auth/view/login_screen.dart';
+import 'package:tasky_app/core/constant/assets_constant.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -9,13 +12,35 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen> {
   @override
+  void initSate(){
+    super.initState();
+    Future.delayed(Duration(seconds: 2),() {
+      Navigator.pushReplacementNamed(context, LoginScreen.routeName);
+    });
+  }
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Color(0xff5F33E1),
-      body: Row(
-        children: [
+    return Scaffold(
+      backgroundColor: const Color(0xff5F33E1),
+      body: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            FadeInLeft(
+              duration: Duration(milliseconds: 900),
+              child: Image.asset(
+                  AssetsConstant.TaskIcon),
+            ),
+            BounceInDown(
+              delay: Duration(milliseconds: 900),
+              duration: Duration(milliseconds: 600) ,
+              from: 50,
+              child: Image.asset(
+                  AssetsConstant.YIcon),
+            ),
 
-        ],
+          ],
+        ),
       ),
     );
   }
